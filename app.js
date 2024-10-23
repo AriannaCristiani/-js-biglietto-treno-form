865efd8a27cc9d2f5e29d1dfc91f369f91ba3dee
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-23 16:30:42 +0200

    prova per collegare le informazioni condizionali

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 
 //chiedere all'utente il suo nome ed il suo cognome
-const userNameSurname = document.getElementById ('name-surname');
+const userNameSurname = document.getElementById('name-surname');
 //console.log(userNameSurname)
 
 
 //chiedere all'utente la sua età
-const age = document.getElementById ('age');
+const age = document.getElementById('age');
 //console.log(age);
 
 //chiedere all'utente il numero di km da percorrere
-const km = document.getElementById ('km');
+const km = document.getElementById('km');
 //console.log(km)
 
 
-//se l'utente inoltra dati non validi
-const dataIsValid = !isNaN(km) && !isNaN(age)
-const dataIsBiggerThanZero = km >= 0 && age >= 0
+
 
 //impedire alla pagina di inviare i dati
 const formElement = document.getElementById('form-element');
@@ -25,10 +23,48 @@ formElement.addEventListener('submit', function (event) {
     event.preventDefault();
 
     console.log(userNameSurname.value)
-    console.log(km.value)
-    console.log(age.value)
+
+    const ageValue = age.value
+    //console.log(ageValue)
+    const kmValue = km.value
+    //console.log(kmValue)
+
+    const dataIsValid = !isNaN(kmValue) && !isNaN(ageValue)
+    const dataIsBiggerThanZero = kmValue >= 0 && ageValue >= 0
 
 
+    if (dataIsValid && dataIsBiggerThanZero) {
+        const basePrice = kmValue * 0.21
+        console.log('prezzo base', basePrice.valueOf)
+
+        let discountPercentage
+
+        if (ageValue < 18) {
+            discountPercentage = 20
+        } else if (ageValue > 65) {
+            discountPercentage = 40
+        } else {
+            discountPercentage = 0
+        }
+
+        console.log('Percentuale sconto: ', discountPercentage)
+        const discount = (basePrice * discountPercentage) / 100
+        console.log('sconto: ', discount.valueOf)
+
+        const price = basePrice - discount
+
+        //stampare il prezzo del biblietto
+        const formattedPrice = new Intl.NumberFormat('it-IT', {
+            style: 'currency',
+            currency: 'EUR',
+        }).format(price)
+
+        const humanPrice = price.toFixed(2)
+
+        console.log('prezzo: ', humanPrice.value + ' €')
+    } else {
+        // alert('I dati inseriti non solo validi')
+    }
 })
 
 
@@ -37,39 +73,6 @@ const buttonSubmit = document.getElementById('button-submit');
 
 
 
-//calcolare prezzo biglietto:
-if (dataIsValid && dataIsBiggerThanZero) {
-    const basePrice = km * 0.21
-    console.log('prezzo base', basePrice)
-
-    let discountPercentage
-
-    if (age < 18) {
-        discountPercentage = 20
-    } else if (age > 65) {
-        discountPercentage = 40
-    } else {
-        discountPercentage = 0
-    }
-
-    console.log('Percentuale sconto: ', discountPercentage)
-    const discount = (basePrice * discountPercentage) / 100
-    console.log('sconto: ', discount)
-
-    const price = basePrice - discount
-
-    //stampare il prezzo del biblietto
-    const formattedPrice = new Intl.NumberFormat('it-IT', {
-        style: 'currency',
-        currency: 'EUR',
-    }).format(price)
-
-    const humanPrice = price.toFixed(2)
-
-    console.log('prezzo: ', humanPrice + ' €')
-} else {
-    // alert('I dati inseriti non solo validi')
-}
 
 
 
