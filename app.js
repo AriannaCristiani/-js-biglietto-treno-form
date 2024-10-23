43acaa4873d6702a2dd6dd3b7e3af261c64c296d
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-23 14:44:26 +0200

    creazione html e inserimento esercizio precedentemente eseguito, con modifica sui prompt che adesso dovranno collegarsi all'html

---
+++ app.js
@@ -0,0 +1,52 @@
+
+//chiedere all'utente il suo nome ed il suo cognome
+const userNameSurname = document.getElementsByClassName('name-surname');
+console.log(userNameSurname)
+
+
+//chiedere all'utente la sua età
+const age = document.getElementsByClassName('age');
+console.log(age);
+
+//chiedere all'utente il numero di km da percorrere
+const km = document.getElementsByClassName('km');
+console.log(km)
+
+
+//se l'utente inoltra dati non validi
+const dataIsValid = !isNaN(km) && !isNaN(age) 
+const dataIsBiggerThanZero = km >= 0 && age >= 0 
+
+//calcolare prezzo biglietto:
+if (dataIsValid && dataIsBiggerThanZero) {
+	const basePrice = km * 0.21
+	console.log('prezzo base', basePrice)
+
+	let discountPercentage
+
+	if (age < 18) {
+		discountPercentage = 20
+	} else if (age > 65) {
+		discountPercentage = 40
+	} else {
+		discountPercentage = 0
+	}
+
+	console.log('Percentuale sconto: ', discountPercentage)
+	const discount = (basePrice * discountPercentage) / 100
+	console.log('sconto: ', discount)
+
+	const price = basePrice - discount
+
+//stampare il prezzo del biblietto
+	const formattedPrice = new Intl.NumberFormat('it-IT', {
+		style: 'currency',
+		currency: 'EUR',
+	}).format(price)
+
+	const humanPrice = price.toFixed(2)
+
+	console.log('prezzo: ', humanPrice + ' €')
+} else {
+	alert('I dati inseriti non solo validi')
+}
